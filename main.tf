698283bd3e44e4312212174c947a49f92f587fdd
User <user@example.com>
2025-03-21 19:17:53 +0200

    test51

--- main.tf
+++ main.tf
@@ -209,4 +209,13 @@ resource "aws_route53_record" "www_a_record" {
   type    = "A"
   ttl     = "300"
   records = [aws_instance.app_server.public_ip]
+}
+
+# Create ArgoCD subdomain A record
+resource "aws_route53_record" "argocd_a_record" {
+  zone_id = data.aws_route53_zone.tradevisdemo.zone_id
+  name    = "argocd.tradevis.click"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.app_server.public_ip]
 } 
